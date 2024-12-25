303b27b7d887fdf8839d4ef1b0db67f1db9f5c46
Abdur Rahman <abdurrahman22224@gmail.com>
2024-12-25 19:46:28 +0530

    Update docker file

--- scrape.py
+++ scrape.py
@@ -7,14 +7,22 @@ from selenium.webdriver.common.alert import Alert
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import json
+from selenium.webdriver.chrome.options import Options
 
 def download_driver():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Ensure headless mode for servers
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.binary_location = "/usr/bin/google-chrome"  # Explicit Chrome binary path
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=chrome_options,
+    )
     return driver
+
 
 # Function to generate roll number based on inputs
 def generate_roll_number(branch, year, course, i):
